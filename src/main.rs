2efe8894290c5c6c8d423192b352465cb55633cd
austin <austinchang4@gmail.com>
2022-08-15 11:22:09 +0800

    feat: capnproto server

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ pub mod tinykv_capnp {
     include!(concat!(env!("OUT_DIR"), "/tinykv_capnp.rs"));
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    env_logger::init();
+    rpc::server::start().await?;
+    Ok(())
 }
--- src/rpc/server.rs
+++ src/rpc/server.rs
@@ -1,3 +1,8 @@
+use futures_util::io::AsyncReadExt;
+use std::net::TcpListener;
+
+use capnp_rpc::{rpc_twoparty_capnp, twoparty::VatNetwork, RpcSystem};
+
 use crate::tinykv_capnp;
 
 struct TinyKVServer;
@@ -22,4 +27,28 @@ impl tinykv_capnp::tiny_k_v::Server for TinyKVServer {
             "method tiny_k_v::Server::get not implemented".to_string(),
         ))
     }
+}
+
+pub async fn start() -> Result<(), anyhow::Error> {
+    let addr = "0.0.0.0:8321";
+    let listener = TcpListener::bind(addr)?;
+    log::info!("Listening on {}", addr);
+    let (stream, _) = listener.accept()?;
+    stream.set_nonblocking(true)?;
+    let stream = tokio::net::TcpStream::from_std(stream)?;
+    let (read_half, write_half) =
+        tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
+
+    let network = VatNetwork::new(
+        read_half,
+        write_half,
+        rpc_twoparty_capnp::Side::Server,
+        Default::default(),
+    );
+
+    let client: tinykv_capnp::tiny_k_v::Client = capnp_rpc::new_client(TinyKVServer);
+
+    let _ = RpcSystem::new(Box::new(network), Some(client.clone().client)).await?;
+
+    Ok(())
 }
