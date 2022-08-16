74300b7be5ff6de81cd8ab7bb1838a0a24696915
austin <austinchang4@gmail.com>
2022-08-16 14:28:58 +0800

    feat: implement get value from store

--- src/rpc/server.rs
+++ src/rpc/server.rs
@@ -6,7 +6,7 @@ use std::net::TcpListener;
 use capnp_rpc::{pry, rpc_twoparty_capnp, twoparty::VatNetwork, RpcSystem};
 
 use crate::{
-    kv::{self, Dataset, KvStore},
+    kv::{self, Dataset, Key, KvStore},
     tinykv_capnp,
 };
 
@@ -48,12 +48,26 @@ impl tinykv_capnp::tiny_k_v::Server for TinyKVServer {
 
     fn get(
         &mut self,
-        _: tinykv_capnp::tiny_k_v::GetParams,
-        _: tinykv_capnp::tiny_k_v::GetResults,
+        params: tinykv_capnp::tiny_k_v::GetParams,
+        mut results: tinykv_capnp::tiny_k_v::GetResults,
     ) -> capnp::capability::Promise<(), capnp::Error> {
-        capnp::capability::Promise::err(capnp::Error::unimplemented(
-            "method tiny_k_v::Server::get not implemented".to_string(),
-        ))
+        let req = pry!(params.get());
+        if !req.has_key() {
+            return Promise::err(capnp::Error::failed("missing key".to_owned()));
+        }
+        let key = req.get_key().unwrap();
+
+        let kkey = key.to_string().clone();
+        let req = self.kv.send(Key(key.to_owned()));
+        capnp::capability::Promise::from_future(async move {
+            if let Ok(Ok(data)) = req.await {
+                results.get().set_key(&kkey);
+                results.get().set_value(&data.to_vec());
+            } else {
+                return Err(capnp::Error::failed("failed to get data".to_owned()));
+            };
+            Ok(())
+        })
     }
 }
 
